0a6021b2980284cd04206eedc150053dc4e6e0e4
Arielle Chongco <achongco@uci.edu>
2017-01-20 20:03:55 -0800

    bullet tag and other stuff

---
+++ Assets/Scripts/BulletDeflectorScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDeflectorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Temp/TEMP_EnemyReact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEMP_EnemyReact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ReceiveDamage(int damage)
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+}
