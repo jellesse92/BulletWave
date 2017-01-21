e6350490b9f27ac22f780416fab6ced4b01113de
asiddiq1 <siddiqaa@uci.edu>
2017-01-20 20:17:40 -0800

    PlayerMovement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed; 
+    float playerMoveHorizontal;
+    float playerMoveVertical;
+    Vector3 position; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerMoveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        playerMoveVertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        position = new Vector3(playerMoveHorizontal, playerMoveVertical, 0); 
+        transform.position += position; 
+       
+
+
+
+    }
+}
