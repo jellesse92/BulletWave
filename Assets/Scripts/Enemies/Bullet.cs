ee76b36eab5798d169c06c85e7431112435e42ef
jellesse92 <jehunter@uci.edu>
2017-01-21 11:39:32 -0800

    Deflecting bullet capability

--- Assets/Scripts/Enemies/Bullet.cs
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -7,13 +7,14 @@ public class Bullet : MonoBehaviour {
     public GameObject bullet;
     public float speed;
     public Vector2 direction;
+    public bool deflected;
     public float damage;
     public int type; // 0 = fireball, 1 = bullet
     public int color; // 0 = r, 1 = g, 2 = b
 
 	// Use this for initialization
 	void Start () {
-
+        deflected = false;
 	}
 	
 	// Update is called once per frame
@@ -21,4 +22,10 @@ public class Bullet : MonoBehaviour {
     {
         bullet.transform.Translate(direction * Time.deltaTime * speed);
 	}
+
+    public void Deflect()
+    {
+        direction = direction * -1;
+        deflected = true;
+    }
 }
