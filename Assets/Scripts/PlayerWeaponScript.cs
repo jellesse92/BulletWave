33ab1f8c33ecf0ef6299a2296f7227eb74a8a776
Arielle Chongco <achongco@uci.edu>
2017-01-20 20:55:56 -0800

    can i push?

--- Assets/Scripts/PlayerWeaponScript.cs
+++ Assets/Scripts/PlayerWeaponScript.cs
@@ -27,9 +27,19 @@ public class PlayerWeaponScript : MonoBehaviour {
             //ActivateReflector();
             ExecuteAttack1();
         }
+
+        //TIER 1!!!
         Color color = Color.green;
 
         Debug.DrawRay(transform.position, transform.up * TIER1_ATTACK_LEN, color);
+        Debug.DrawRay(transform.position, (transform.up + new Vector3(1f,0f,0f)) * TIER1_ATTACK_LEN, color);
+
+
+        Debug.DrawRay(transform.position, (transform.up + new Vector3(-1f, 0f, 0f)) * TIER1_ATTACK_LEN, color);
+        Debug.DrawRay(transform.position, (transform.up + new Vector3(.5f, 0f, 0f)) * TIER1_ATTACK_LEN, color);
+        Debug.DrawRay(transform.position, (transform.up + new Vector3(-.5f, 0f, 0f)) * TIER1_ATTACK_LEN, color);
+
+
     }
 
     //REFLECTOR FUNCTIONS
@@ -61,8 +71,14 @@ public class PlayerWeaponScript : MonoBehaviour {
     {
         RaycastHit2D[][] rays = new RaycastHit2D[5][];
         rays[0] = Physics2D.RaycastAll(transform.position, transform.up, TIER1_ATTACK_LEN, layermask);
+        rays[1] = Physics2D.RaycastAll(transform.position, (new Vector2(10f, 10f)), 100f, layermask);
 
         foreach (RaycastHit2D targets in rays[0])
+            if (targets)
+                if (targets.collider.tag == "Enemy")
+                    targets.transform.GetComponent<TEMP_EnemyReact>().ReceiveDamage(5);
+
+        foreach (RaycastHit2D targets in rays[1])
             if (targets)
                 if (targets.collider.tag == "Enemy")
                     targets.transform.GetComponent<TEMP_EnemyReact>().ReceiveDamage(5);
