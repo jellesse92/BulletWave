cac3dec1eb4a9d9abe26b48c58aaed890b02bd8a
Nick Smith <smithne@uci.edu>
2017-01-21 22:22:28 -0800

    all the spawners

---
+++ Assets/Scripts/Enemies/Spawner3.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner3 : MonoBehaviour {
+
+    public GameObject senemy;
+    public int senemyNum;
+    public List<GameObject> senemyList;
+    public bool isCoolingDown;
+    public float coolDownTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        senemyList = new List<GameObject>();
+        for (int i = 0; i < senemyNum; i++)
+        {
+            GameObject b = (GameObject)Instantiate(senemy);
+            b.SetActive(false);
+            senemyList.Add(b);
+        }
+        isCoolingDown = false;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isCoolingDown)
+        {
+            coolDownTime = Random.Range(.5f, 1.5f);
+            Spawn();
+            isCoolingDown = true;
+            Invoke("CoolDownSpawn", coolDownTime);
+        }
+    }
+
+    private void CoolDownSpawn()
+    {
+        isCoolingDown = false;
+    }
+
+    public void Spawn()
+    {
+        for (int i = 0; i < senemyNum; i++)
+        {
+            if (!senemyList[i].activeInHierarchy)
+            {
+                PulsingSpiralEnemy b = senemyList[i].GetComponentInChildren<PulsingSpiralEnemy>();
+                senemyList[i].transform.position = transform.position;
+                b.attackRadius = Random.Range(7f, 13f);
+                b.projectileWaveType = (int)Random.Range(0.0f, 3f);
+                if (b.projectileWaveType == 0)
+                {
+                    b.GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.red);
+                }
+                if (b.projectileWaveType == 1)
+                {
+                    b.GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.green);
+                }
+                if (b.projectileWaveType == 2)
+                {
+                    b.GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.blue);
+                }
+                senemyList[i].SetActive(true);
+
+                break;
+            }
+        }
+    }
+}
