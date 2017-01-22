016b377d8b2e26ff7d7368b652c18fb1b31e4831
Nick Smith <smithne@uci.edu>
2017-01-22 11:36:17 -0800

    spawner stuff
    spawners rotate now, and you can change the spawn time range from the
    editor

--- Assets/Scripts/Enemies/Spawner3.cs
+++ Assets/Scripts/Enemies/Spawner3.cs
@@ -10,6 +10,13 @@ public class Spawner3 : MonoBehaviour {
     public bool isCoolingDown;
     public float coolDownTime;
 
+    public Vector3 rotationMask; //which axes to rotate around
+    public float rotationSpeed;  //degrees per second
+    public GameObject rotateAroundObject;
+
+    public float spawnMin = 2;
+    public float spawnMax = 5;
+
     // Use this for initialization
     void Start()
     {
@@ -21,15 +28,32 @@ public class Spawner3 : MonoBehaviour {
             senemyList.Add(b);
         }
         isCoolingDown = false;
+
+        rotationMask = new Vector3(0, 0, 1);
+        rotationSpeed = 5;
+        rotateAroundObject = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (rotateAroundObject)
+        {//If true in the inspector orbit <rotateAroundObject>:
+            transform.RotateAround(rotateAroundObject.transform.position,
+            rotationMask, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {//not set -> rotate around own axis/axes:
+            transform.Rotate(new Vector3(
+            rotationMask.x * rotationSpeed * Time.deltaTime,
+            rotationMask.y * rotationSpeed * Time.deltaTime,
+            rotationMask.z * rotationSpeed * Time.deltaTime));
+        }
+
         if (!isCoolingDown)
         {
-            coolDownTime = Random.Range(.5f, 1.5f);
+            coolDownTime = Random.Range(spawnMin, spawnMax);
             Spawn();
             isCoolingDown = true;
             Invoke("CoolDownSpawn", coolDownTime);
