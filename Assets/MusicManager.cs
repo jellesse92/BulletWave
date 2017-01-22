d26056eaf1e31aeaef502eded58254de578263de
Arielle Chongco <achongco@uci.edu>
2017-01-22 12:50:04 -0800

    Music loop added

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager Instance
+    {
+        get;
+        set;
+    }
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(transform.gameObject);
+        Instance = this;
+
+        //Destroys copy of this on scene
+        if (FindObjectsOfType(GetType()).Length > 1)
+            Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
